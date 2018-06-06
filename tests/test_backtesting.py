87029677bf8e91f861526c9847dcd89615d0792f
Maricris Esgana <maricris.esgana@gmail.com>
2018-06-07 02:52:32 +0800

    Modified to add tearDown

--- tests/test_backtesting.py
+++ tests/test_backtesting.py
@@ -1,6 +1,10 @@
+import os
+import shutil
 import unittest
 
 import pandas as pd
+
+from poor_trader import config
 from poor_trader.backtesting.backtester import DataFramePortfolio, DataFrameBacktester
 from poor_trader.backtesting.broker import COLFinancial
 from poor_trader.backtesting.entity import Account
@@ -13,6 +17,7 @@ from tests import test_indicator
 
 class TestBacktesting(unittest.TestCase):
     def setUp(self):
+        self.tearDown()
         self.market = csv_to_market('TestMarket', test_indicator.INTRADAY_HISTORICAL_DATA_PATH)
         self.account = Account(100000)
 
@@ -20,11 +25,16 @@ class TestBacktesting(unittest.TestCase):
         atr_channel_breakout = ATRChannelBreakout(indicator_factory, sma=10, fast=5, slow=10)
 
         self.portfolio = DataFramePortfolio(account=self.account,
+                                            dir_path=config.TEST_TEMP_PATH,
                                             indicators_dir_path=test_indicator.TEMP_INDICATORS_PATH,
                                             market=self.market,
                                             position_sizing=EquityPercentage(market=self.market),
                                             broker=COLFinancial(),
                                             strategies=[atr_channel_breakout])
+
+    def tearDown(self):
+        if os.path.exists(config.TEST_TEMP_PATH):
+            shutil.rmtree(config.TEST_TEMP_PATH)
 
     def test_backtester(self):
         backtester = DataFrameBacktester(self.portfolio)
