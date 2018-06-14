2212c99d625b9de75ae97af56628cb918d79e9a6
Maricris Esgana <maricris.esgana@gmail.com>
2018-06-15 06:50:08 +0800

    Modified to use boardlot

--- poor_trader/backtesting/position_sizing.py
+++ poor_trader/backtesting/position_sizing.py
@@ -1,3 +1,4 @@
+from poor_trader import utils
 from poor_trader.market import Market
 from poor_trader.backtesting.entity import PositionSizing
 
@@ -9,12 +10,16 @@ class EquityPercentage(PositionSizing):
         self.total_risk_pct = total_risk_pct
         self.unit_risk = unit_risk
 
-    def calculate_shares(self, date, symbol, account):
+    def calculate_shares(self, date, symbol, account, use_boardlot=True):
         price = self.market.get_close(date, symbol)
         C = account.equity * self.total_risk_pct
         R = price * self.unit_risk
         P = C / R
-        return int(P)
+        shares = int(P)
+        if use_boardlot:
+            boardlot = utils.boardlot(price)
+            shares = int(shares / boardlot) * boardlot
+        return shares
 
     def calculate_total_risk(self, price, shares, account):
         R = price * self.unit_risk
