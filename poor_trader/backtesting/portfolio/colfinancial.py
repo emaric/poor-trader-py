6f6d228bf8a4850b1adf705581ac90a55fbd5987
Maricris Esgana <maricris.esgana@gmail.com>
2018-06-23 06:49:10 +0800

    Renamed default portfolio implementation
    Added save_dir_path attribute to set the location for the backtest results: equity_curve.csv, transaction.csv, position.csv

--- poor_trader/backtesting/portfolio/colfinancial.py
+++ poor_trader/backtesting/portfolio/colfinancial.py
@@ -1,3 +1,5 @@
+import os
+
 import pandas as pd
 
 from poor_trader import config
@@ -7,16 +9,18 @@ from poor_trader.market import Market
 from poor_trader.screening.entity import Direction
 
 
-class COLFinancialPortfolio(Portfolio):
+class DefaultPortfolio(Portfolio):
 
     def __init__(self, account: Account, market: Market, broker: Broker, position_sizing: PositionSizing,
-                 equity_curve: EquityCurve, strategies=list()):
-        super().__init__(account=account, equity_curve=equity_curve, name=self.__class__.__name__, strategies=strategies)
+                 equity_curve: EquityCurve, strategies=list(), name=None, save_dir_path=None):
+        super().__init__(account=account, equity_curve=equity_curve, name=name or self.__class__.__name__, strategies=strategies)
         self.market = market
         self.broker = broker
         self.position_sizing = position_sizing
         self.transaction_service = TransactionService()
         self.position_service = PositionService()
+        self.save_dir_path = save_dir_path or config.generate_backtesting_results_dir_path()
+        print('!!!Saving backtest results to ' + self.save_dir_path)
 
     def update_account_on_close(self, exit_value):
         self.account.cash = self.account.cash + exit_value
@@ -83,11 +87,11 @@ class COLFinancialPortfolio(Portfolio):
         self.update_open_positions_values(date)
         self.update_account_on_open(exit_value=self.position_service.get_open_values())
         self.equity_curve.update(date, self.account)
-        print(pd.to_datetime(date).strftime(config.DATE_FORMAT),
+        print(pd.to_datetime(date).strftime(config.DATETIME_FORMAT),
               '{:>18.4f}'.format(self.equity_curve.get_equity(date)),
               '{:>18.4f}'.format(self.equity_curve.get_cash(date)),
               '{:>13.4f}'.format(self.equity_curve.get_drawdown_percent(date)))
-        self.save(config.RESOURCES_PATH)
+        self.save(self.save_dir_path)
 
     def get_positions(self):
         return self.position_service.get_open_positions()
@@ -111,18 +115,27 @@ if __name__ == '__main__':
     from poor_trader.screening.indicator import PickleIndicatorFactory
     from poor_trader.screening.strategy import DonchianChannel, ATRChannelBreakout
 
+    investa_symbols = ['ALI', 'AC',    'MBT', 'SM',    'SMPH', 'JFC',   'URC',   'MPI', 'ICT',  'BLOOM',
+                       'BPI', 'NOW',   'TEL', 'GTCAP', 'VITA', 'JGS',   'SECB',  'MER', 'AGI',  'HOUSE',
+                       'AEV', 'PGOLD', 'DMC', 'MEG',   'LTG',  'GLO',   'MWIDE', 'MRC', 'RLC',  'MAC',
+                       'SCC', 'PXP',   'AP',  'VLL',   'FB',   'EAGLE', 'WLCON', 'LR',  'SMC',  'RWM',
+                       'DNL', 'MRP',   'CHP', 'IMI',   'FNI',  'EW',    'VUL',   'CLC', 'PCOR', 'BRN',
+                       'LPZ', 'ANI',   'FLI', 'DD',    'PRMX', 'ATN',   'APX',   'CPM', 'TUGS', 'PIZZA',
+                       'X',   'POPI',  'CPG', 'NIKL',  'ION']
+
     INDICATORS_PATH = config.TEMP_PATH / 'indicators'
     HISTORICAL_DATA_PATH = config.RESOURCES_PATH / 'historical_data.pkl'
 
-    pse_market = pkl_to_market('PSE', HISTORICAL_DATA_PATH)
+    pse_market = pkl_to_market('PSE', HISTORICAL_DATA_PATH, symbols=investa_symbols)
     strategies = [DonchianChannel(PickleIndicatorFactory(INDICATORS_PATH, market=pse_market), fast=100, slow=120),
                   ATRChannelBreakout(PickleIndicatorFactory(INDICATORS_PATH, market=pse_market), fast=100, slow=120)]
 
-    colport = COLFinancialPortfolio(account=Account(1000000),
-                                    market=pse_market,
-                                    broker=COLFinancial(),
-                                    position_sizing=EquityPercentage(pse_market),
-                                    equity_curve=DefaultEquityCurve(),
-                                    strategies=strategies)
+    colport = DefaultPortfolio(account=Account(1000000),
+                               market=pse_market,
+                               broker=COLFinancial(),
+                               position_sizing=EquityPercentage(pse_market),
+                               equity_curve=DefaultEquityCurve(),
+                               strategies=strategies,
+                               name='test3')
     default = DataFrameBacktester(colport)
-    ec = default.run(pse_market, start=pd.to_datetime('2013-01-01'))
+    ec = default.run(pse_market, start=pd.to_datetime('2017-08-20'), end=pd.to_datetime('2017-11-23'))
