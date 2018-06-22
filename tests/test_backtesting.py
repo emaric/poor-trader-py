c86a2d9e7032fb8a6d1519107da6d9ec63daadf2
Maricris Esgana <maricris.esgana@gmail.com>
2018-06-23 06:53:57 +0800

    Fixed test_backtesting

--- tests/test_backtesting.py
+++ tests/test_backtesting.py
@@ -5,9 +5,11 @@ import unittest
 import pandas as pd
 
 from poor_trader import config
-from poor_trader.backtesting.backtester import DataFramePortfolio, DataFrameBacktester
+from poor_trader.backtesting.backtester import DataFrameBacktester
 from poor_trader.backtesting.broker import COLFinancial
 from poor_trader.backtesting.entity import Account
+from poor_trader.backtesting.equity_curve import DefaultEquityCurve
+from poor_trader.backtesting.portfolio.colfinancial import DefaultPortfolio
 from poor_trader.backtesting.position_sizing import EquityPercentage
 from poor_trader.market import csv_to_market
 from poor_trader.screening.indicator import PickleIndicatorFactory
@@ -24,13 +26,12 @@ class TestBacktesting(unittest.TestCase):
         indicator_factory = PickleIndicatorFactory(test_indicator.TEMP_INDICATORS_PATH, self.market)
         atr_channel_breakout = ATRChannelBreakout(indicator_factory, sma=10, fast=5, slow=10)
 
-        self.portfolio = DataFramePortfolio(account=self.account,
-                                            dir_path=config.TEST_TEMP_PATH,
-                                            indicators_dir_path=test_indicator.TEMP_INDICATORS_PATH,
-                                            market=self.market,
-                                            position_sizing=EquityPercentage(market=self.market),
-                                            broker=COLFinancial(),
-                                            strategies=[atr_channel_breakout])
+        self.portfolio = DefaultPortfolio(account=self.account,
+                                          market=self.market,
+                                          position_sizing=EquityPercentage(market=self.market),
+                                          broker=COLFinancial(),
+                                          equity_curve=DefaultEquityCurve(),
+                                          strategies=[atr_channel_breakout])
 
     def tearDown(self):
         if os.path.exists(config.TEST_TEMP_PATH):
@@ -39,8 +40,8 @@ class TestBacktesting(unittest.TestCase):
     def test_backtester(self):
         backtester = DataFrameBacktester(self.portfolio)
         equity_curve = backtester.run(self.market)
-        print(equity_curve)
-        self.assertTrue(pd.Index.equals(equity_curve.index, self.market.__df_historical_data__.index))
+        print(equity_curve.get_equity())
+        self.assertTrue(pd.Index.equals(equity_curve.get_equity()[1:].index, self.market.__df_historical_data__.index))
 
 
 if __name__ == '__main__':
