8315e1363ce37b98630307d07fab80ea7457b629
Maricris Esgana <maricris.esgana@gmail.com>
2018-06-23 06:56:25 +0800

    Removed test_screener and moved the test_screener method into test_screening.py

--- tests/test_screening.py
+++ tests/test_screening.py
@@ -7,6 +7,7 @@ import pandas as pd
 from poor_trader import config
 from poor_trader.market import csv_to_market
 from poor_trader.screening.indicator import PickleIndicatorRunnerFactory, IndicatorRunner, PickleIndicatorFactory
+from poor_trader.screening.screener import DataFrameScreener
 
 INDICATORS_PATH = config.TEST_TEMP_PATH / 'indicators'
 HISTORICAL_DATA_PATH = config.TEST_RESOURCES_PATH / 'intraday_historical_data.csv'
@@ -44,6 +45,16 @@ class TestScreening(unittest.TestCase):
                 df_quotes = self.market.get_quotes(symbol=symbol)
                 indices = indicator_instance.get_indices(symbol=symbol)
                 self.assertSetEqual(set(df_quotes.index.values), set(indices), symbol)
+
+    def test_screener(self):
+        screener = DataFrameScreener(self.market, INDICATORS_PATH)
+        df_long, df_short = screener.scan()
+        screener.print('LONG', df_long)
+
+        indicators = screener.create_indicators()
+        self.assertTrue(len(indicators) > 0)
+        for i in indicators:
+            self.assertTrue(i.name in df_long.columns)
 
 
 if __name__ == '__main__':
