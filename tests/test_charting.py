d9108ed8b786a91c44e8705430030ff4c68f7a70
Maricris Esgana <maricris.esgana@gmail.com>
2018-06-23 08:53:54 +0800

    Created test_charting

--- tests/test_charting.py
+++ tests/test_charting.py
@@ -1,9 +1,27 @@
 import unittest
 
+from poor_trader import config
+from poor_trader.charting import equity_curve, transactions
+from poor_trader.market import csv_to_market
+from tests import test_indicator
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+BACKTEST_RESULT_PATH = config.TEST_RESOURCES_PATH / 'backtest_result'
+
+EQUITY_CURVE_PATH = BACKTEST_RESULT_PATH / config.EQUITY_CURVE_FILENAME
+
+TRANSACTIONS_PATH = BACKTEST_RESULT_PATH / config.TRANSACTIONS_FILENAME
+
+
+class TestCharting(unittest.TestCase):
+    def setUp(self):
+        self.market = csv_to_market('TestMarket', test_indicator.INTRADAY_HISTORICAL_DATA_PATH)
+
+    def test_equity_curve(self):
+        equity_curve.csv_to_chart(EQUITY_CURVE_PATH)
+
+    def test_transactions(self):
+        transactions.csv_to_chart(self.market, TRANSACTIONS_PATH)
+        self.fail('TODO: Add indicators based on the transaction tags.')
 
 
 if __name__ == '__main__':
