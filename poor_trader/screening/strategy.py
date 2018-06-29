dece350a7a0c6871e8ec29b35a7e0efc1afbeab5
Maricris Esgana <maricris.esgana@gmail.com>
2018-06-30 00:14:28 +0800

    Renamed Strategy to DefaultStrategy

--- poor_trader/screening/strategy.py
+++ poor_trader/screening/strategy.py
@@ -5,7 +5,7 @@ from poor_trader.screening import entity, indicator
 from poor_trader.screening.entity import Direction
 
 
-class Strategy(entity.Strategy):
+class DefaultStrategy(entity.Strategy):
     __metaclass__ = abc.ABCMeta
 
     def __init__(self, indicator_factory: indicator.IndicatorFactory):
@@ -16,8 +16,23 @@ class Strategy(entity.Strategy):
     def __init_indicators__(self):
         raise NotImplementedError
 
+    def entry_condition(self, date, symbol, market: Market, direction=Direction.LONG):
+        if direction == Direction.LONG:
+            return self.is_long(date, symbol)
+        if direction == Direction.SHORT:
+            return self.is_short(date, symbol)
 
-class ATRChannelBreakout(Strategy):
+    def reentry_condition(self, date, symbol, market: Market, direction=Direction.LONG):
+        raise NotImplementedError
+
+    def exit_condition(self, date, symbol, market: Market, entry_date, direction=Direction.LONG):
+        exit_direction = Direction.SHORT if direction == Direction.LONG else Direction.LONG
+        exit_tags = self.get_indicator_names(direction=exit_direction, date=date, symbol=symbol, start=entry_date)
+        entry_tags = self.get_indicator_names(direction=direction, date=date, symbol=symbol, start=entry_date)
+        return len([_ for _ in entry_tags if _ not in exit_tags]) <= 0
+
+
+class ATRChannelBreakout(DefaultStrategy):
     def __init__(self, indicator_factory: indicator.IndicatorFactory, top=7, bottom=3, sma=150, fast=100, slow=120):
         super().__init__(indicator_factory)
         self.top = top
@@ -32,23 +47,8 @@ class ATRChannelBreakout(Strategy):
         ma_cross = self.indicator_factory.create(indicator.MACross, fast=self.fast, slow=self.slow)
         return [atr_channel, ma_cross]
 
-    def entry_condition(self, date, symbol, market: Market, direction=Direction.LONG):
-        if direction == Direction.LONG:
-            return self.is_long(date, symbol)
-        if direction == Direction.SHORT:
-            return self.is_short(date, symbol)
 
-    def reentry_condition(self, date, symbol, market: Market, direction=Direction.LONG):
-        raise NotImplementedError
-
-    def exit_condition(self, date, symbol, market: Market, entry_date, direction=Direction.LONG):
-        exit_direction = Direction.SHORT if direction == Direction.LONG else Direction.LONG
-        exit_tags = self.get_indicator_names(direction=exit_direction, date=date, symbol=symbol, start=entry_date)
-        entry_tags = self.get_indicator_names(direction=direction, date=date, symbol=symbol, start=entry_date)
-        return len([_ for _ in entry_tags if _ not in exit_tags]) <= 0
-
-
-class DonchianChannel(Strategy):
+class DonchianChannel(DefaultStrategy):
     def __init__(self, indicator_factory: indicator.IndicatorFactory, high=50, low=50, fast=100, slow=120):
         super().__init__(indicator_factory)
         self.high = high
@@ -62,18 +62,18 @@ class DonchianChannel(Strategy):
         ma_cross = self.indicator_factory.create(indicator.MACross, fast=self.fast, slow=self.slow)
         return [donchian_channel, ma_cross]
 
-    def entry_condition(self, date, symbol, market: Market, direction=Direction.LONG):
-        if direction == Direction.LONG:
-            return self.is_long(date, symbol)
-        if direction == Direction.SHORT:
-            return self.is_short(date, symbol)
 
-    def reentry_condition(self, date, symbol, market: Market, direction=Direction.LONG):
-        raise NotImplementedError
+class TrendStrength(DefaultStrategy):
+    def __init__(self, indicator_factory: indicator.IndicatorFactory, start=40, end=150, step=5, fast=100, slow=150):
+        super().__init__(indicator_factory)
+        self.start = start
+        self.end = end
+        self.step = step
+        self.fast = fast
+        self.slow = slow
+        self.indicators = self.__init_indicators__() if len(self.indicators) <= 0 else self.indicators
 
-    def exit_condition(self, date, symbol, market: Market, entry_date, direction=Direction.LONG):
-        exit_direction = Direction.SHORT if direction == Direction.LONG else Direction.LONG
-        exit_tags = self.get_indicator_names(direction=exit_direction, date=date, symbol=symbol, start=entry_date)
-        entry_tags = self.get_indicator_names(direction=direction, date=date, symbol=symbol, start=entry_date)
-        return len([_ for _ in entry_tags if _ not in exit_tags]) <= 0
-
+    def __init_indicators__(self):
+        trend_strength = self.indicator_factory.create(indicator.TrendStrength, start=self.start, end=self.end, step=self.step)
+        ma_cross = self.indicator_factory.create(indicator.MACross, fast=self.fast, slow=self.slow)
+        return [trend_strength, ma_cross]
